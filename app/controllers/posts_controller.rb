1021e4d3099992765d9fba2cec981a8a3426121f
kakeru <okinawan.kkr0713@icloud.com>
2020-03-07 19:34:48 +0900

    view修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,82 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  # before_action :authenticate_user!, only: [:new, :edit, :update, :destroy, :render_index]
+
   def index
+    @posts = Post.all#.per(12)
   end
 
-  def new
-  end
+  # def search
+  #    @q = Post.search(search_params)
+  #    @posts = @q.result(distinct: true)
+  # end
 
   def show
   end
 
+  def new
+    if params[:back]
+      @post = Post.new(post_params)
+    else
+      @post = Post.new
+    end
+  end
+
   def edit
   end
+
+  def create
+    # @post = current_user.posts.build(post_params)
+    @post = Post.new(post_params)
+
+    if params[:back]
+        render :new
+      else
+      if @post.save
+        redirect_to posts_path, notice:"スレッドを作成しました！"
+      else
+        render :new
+      end
+    end
+  end
+
+
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "スレッドを更新しました！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    # if current_user.id == @post.user.id
+      @post.destroy
+      redirect_to posts_path, notice:"スレッドを削除しました！"
+    # end
+  end
+
+  def new_guest
+    user = User.find_or_create_by!(email: 'guest@example.com',name: "ゲスト太郎") do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.confirmed_at = Time.now
+    end
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+  def search_params
+    params.require(:q).permit!
+  end
+
 end
