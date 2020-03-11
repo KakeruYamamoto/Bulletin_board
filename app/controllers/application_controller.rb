4cf3c78ecf03ca88bde0715b4111d2d01589c093
kakeru <okinawan.kkr0713@icloud.com>
2020-03-11 16:03:05 +0900

    検索機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_search
+  def after_sign_in_path_for(resource)
+    posts_path
+  end
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :icon])
+  end
+  def set_search
+    if params[:q] != nil
+      params[:q]['title_cont_any'] = params[:q]['title_cont_any'].split(/[\p{blank}\s]+/)
+      @q = Post.ransack(params[:q])
+      @posts = @q.result.order(created_at: :desc).page(params[:page]).per(10)
+    else
+      @q = Post.ransack(params[:q])
+      @posts = @q.result(distinct: true).order(created_at: :desc).page(params[:page]).per(10)
+    end
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
 
   def index
-    @posts = Post.page(params[:page]).per(12)
+    @posts = Post.order(created_at: :desc).page(params[:page]).per(10)
   end
 
   def search
